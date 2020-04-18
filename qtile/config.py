12a245f01833339640087ef088bf6319a01cd785
procleap <procleap@users.noreply.github.com>
2020-04-18 12:58:18 +0200

    Delete default help message from bar

--- qtile/config.py
+++ qtile/config.py
@@ -97,8 +97,6 @@ screens = [
                 widget.GroupBox(),
                 widget.Prompt(),
                 widget.WindowName(),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
                 widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
                 widget.QuickExit(),
