d0042e1157759ae357a105da538656a12fe661cb
procleap <procleap@users.noreply.github.com>
2020-04-21 12:12:53 +0200

    Add customized volume widget

--- bar.py
+++ bar.py
@@ -6,6 +6,11 @@ def gpu_temp():
     cmd = ["/usr/bin/nvidia-smi", "--query-gpu=temperature.gpu", "--format=csv,noheader"]
     return subprocess.check_output(cmd).decode('utf-8').strip() + "°C"
 
+def get_volume_level():
+    """  """
+    cmd = ["/usr/bin/pamixer", "--get-volume"]
+    return subprocess.check_output(cmd).decode('utf-8').strip()
+
 # Important: the Memory and CPU widgets require `python-psutil` to be installed
 bar_widgets = [
     widget.Sep(
@@ -63,6 +68,20 @@ bar_widgets = [
     ),
     widget.Memory(
         format = '{MemUsed}M'
+    ),
+    widget.Sep(
+        linewidth = 0,
+        padding = 7,
+    ),
+    widget.TextBox(
+        text = "<span size='x-large'></span>",
+        #text = "   ",
+        font = 'KoHo'
+    ),
+    widget.GenPollText(
+        fmt = '{}%',
+        func = get_volume_level,
+        update_interval = 0.2,
     ),
     widget.Sep(
         linewidth = 0,
