66c6a3438cae6d7f1ad5be36dd7c862051e74a3c
procleap <procleap@users.noreply.github.com>
2020-04-29 16:20:02 +0200

    Enable new key binding

--- config.py
+++ config.py
@@ -26,10 +26,10 @@ for i in groups:
         # mod1 + shift + letter of group = switch to & move focused window to group
         Key([mod], i.name, lazy.window.togroup(i.name, switch_group=True),
             desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+
+        # Move focused window to group
+        Key([mod, "control"], i.name, lazy.window.togroup(i.name),
+            desc="move focused window to group {} ".format(i.name)),
     ])
 
 layouts = [
